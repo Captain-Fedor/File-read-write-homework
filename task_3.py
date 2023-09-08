44199ca63f4a00aa1eb52436a96e3d84509a8d64
Fedor Sumkin <runway067@gmail.com>
2023-09-08 18:09:11 +0300

    continue on task 3

--- task_3.py
+++ task_3.py
@@ -12,5 +12,17 @@ def count_file_lines(file:str):
 for file_name in file_list:
     sort_dict[file_name] = count_file_lines(file_name)
 sort_dict = sorted(sort_dict.items(), key = lambda x: x[1])
+print(sort_dict)
+def writing_file(file: str, length: int):
+    with codecs.open('final_task_3.txt', 'a', encoding='utf-8') as data:
+        file +='\n'
+        data.write(file)
+        data.write(str(length)+'\n')
+#     with codecs.open(file, encoding='utf-8', 'r'):
+#     with codecs.open('final_task_3.txt',encoding='utf-8', 'a'):
+# #         ...
+writing_file('45.txt', 5)
+# for file_data in sort_dict:
+
 
 
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-for x in range(3,1):
+for x in range(3):
     print(x)
